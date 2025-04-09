3263136e05482dd43486250e55c49b8df853248b
Locker <90060146+LockeRL@users.noreply.github.com>
2025-04-09 21:26:10 +0300

    add fonts

---
+++ frontend/composeApp/src/commonMain/kotlin/org/locker/start_android/presentation/App.kt
@@ -0,0 +1,12 @@
+package org.locker.start_android.presentation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import org.locker.start_android.presentation.compose.theme.StartAndroidTheme
+
+@Composable
+fun App() {
+    StartAndroidTheme {
+        Text("Start Android")
+    }
+}
