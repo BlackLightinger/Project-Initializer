20a744cd5e70443e1622c777a2cedd72847e9eb8
Locker <90060146+LockeRL@users.noreply.github.com>
2025-04-09 21:31:08 +0300

    add top bar

--- frontend/composeApp/src/commonMain/kotlin/org/locker/start_android/presentation/App.kt
+++ frontend/composeApp/src/commonMain/kotlin/org/locker/start_android/presentation/App.kt
@@ -1,12 +1,14 @@
 package org.locker.start_android.presentation
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import org.locker.start_android.presentation.compose.panes.start_pane.pane.StartPane
 import org.locker.start_android.presentation.compose.theme.StartAndroidTheme
 
 @Composable
 fun App() {
     StartAndroidTheme {
-        Text("Start Android")
+        StartPane(modifier = Modifier.fillMaxSize())
     }
 }
