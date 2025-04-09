86abb0b72017375cda58d5990938899546786e9e
Locker <90060146+LockeRL@users.noreply.github.com>
2025-04-09 21:43:57 +0300

    add di for viewmodels

--- frontend/composeApp/src/commonMain/kotlin/org/locker/start_android/presentation/App.kt
+++ frontend/composeApp/src/commonMain/kotlin/org/locker/start_android/presentation/App.kt
@@ -3,15 +3,24 @@ package org.locker.start_android.presentation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import org.koin.compose.KoinApplication
+import org.locker.start_android.di.viewModelModule
 import org.locker.start_android.presentation.compose.panes.start_pane.pane.StartPane
 import org.locker.start_android.presentation.compose.theme.StartAndroidTheme
 
 @Composable
 fun App() {
-
-    StartAndroidTheme {
-        StartPane(
-            modifier = Modifier.fillMaxSize()
-        )
+    KoinApplication(
+        application = {
+            modules(
+                viewModelModule
+            )
+        }
+    ) {
+        StartAndroidTheme {
+            StartPane(
+                modifier = Modifier.fillMaxSize()
+            )
+        }
     }
 }
--- frontend/composeApp/src/commonMain/kotlin/org/locker/start_android/presentation/compose/panes/dependencies/pane/DependenciesPane.kt
+++ frontend/composeApp/src/commonMain/kotlin/org/locker/start_android/presentation/compose/panes/dependencies/pane/DependenciesPane.kt
@@ -1,12 +1,14 @@
 package org.locker.start_android.presentation.compose.panes.dependencies.pane
 
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.koin.compose.koinInject
+import org.locker.start_android.presentation.compose.panes.dependencies.viewModel.DependenciesViewModel
 
 
 @Composable
 fun DependenciesPane(
+    dependenciesViewModel: DependenciesViewModel = koinInject(),
     modifier: Modifier = Modifier,
 ) {
 
