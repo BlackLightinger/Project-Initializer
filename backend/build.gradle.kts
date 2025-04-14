6362d84702e18113ddfa474a69d38f442e79e024
Максим Дейнеко <maximdeyneko@Maksims-MacBook-Air.local>
2025-04-14 03:04:21 +0300

    init backend #36

---
+++ backend/build.gradle.kts
@@ -0,0 +1,45 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val koin_version: String by project
+val koin_ktor: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    id("io.ktor.plugin") version "2.3.2"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_ktor")
+    // SLF4J Logger
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_ktor")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    // Koin Test features
+    testImplementation("io.insert-koin:koin-test:$koin_version")
+    // Koin for JUnit 4
+    testImplementation("io.insert-koin:koin-test-junit4:$koin_version")
+    // Koin for JUnit 5
+    testImplementation("io.insert-koin:koin-test-junit5:$koin_version")
+    // Koin for Ktor
+}
